affebb5113129dc6930fc82013d4efe9dfeba897
Vuyani Ncube <nvuyani307@gmail.com>
2022-04-30 23:51:07 +0200

    late change

--- front/tgapp/src/app/pages/layout/header/header.component.ts
+++ front/tgapp/src/app/pages/layout/header/header.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  mega_menu_display = "none";
+
+  catergories = {
+    "Network" : {
+        "Network Cameras" : [ "ColorVu Series", "AcuSense"],
+        "NVR" : ["4-CH NVR", "8-CH NVR"]
+    },
+    "TurboHD" : {},
+    "Display & Control" : {},
+    "Mobile" : {},
+    "Access Control" : {},
+    "Video Intercomm" : {},
+    "Alarm" : {},
+    "Thermal" : {},
+    "Accesories" : {},
+    "Software" : {}
+  };
+
+  subcatergories = {};
+  products = [];
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  mega_menu() {
+
+    if (this.mega_menu_display === "none"){
+      this.mega_menu_display = "block";
+    }
+
+    else{
+      this.mega_menu_display = "none"
+    }
+  }
+
+  subs(catergory:any){
+    this.subcatergories = catergory;
+    this.products = [];
+  }
+
+  prods(products:any){
+    this.products = products
+  }
 }
