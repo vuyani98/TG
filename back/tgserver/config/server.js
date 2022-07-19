a5c1c967982e4130580325be18f161b0b3579b5c
Vuyani Ncube <nvuyani307@gmail.com>
2022-07-19 23:17:49 +0200

    added backend

---
+++ back/tgserver/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2e760edccecaa4bfa449252f5de3d0a0'),
+    },
+  },
+});
---
+++ back/tgserver/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '013d1d0b-a7a2-483d-a57e-b02139324475'
+};
